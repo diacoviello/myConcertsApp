dd720e1295f36241256c44e5684e95a0595bbc65
Dylan Geswelli <dylan.geswelli@gmail.com>
2021-03-19 12:53:43 -0400

    event routes working in insomnia

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,10 @@
+const router = require("express").Router();
 
+// const homeRoutes = require("./home-routes");
+const eventRoutes = require("./eventRoutes");
 
+// router.use("/user", userRoutes);
+router.use("/event", eventRoutes);
+
+module.exports = router;
 
